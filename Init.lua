d2602bb0d2d5941d475d2239ba6339d0f191a214
cygnus404 <cygnus404x@gmail.com>
2020-10-16 21:05:33 +0200

    Add files via upload

---
+++ Init.lua
@@ -0,0 +1,8 @@
+local AddOnName = ...
+local _G = _G
+local LibStub = LibStub
+
+local AF = LibStub('AceAddon-3.0'):NewAddon('Addfonts', 'AceEvent-3.0', 'AceTimer-3.0')
+
+_G.Addfonts = AF
+AF.LSM = LibStub('LibSharedMedia-3.0')
---
+++ Media/Media.lua
@@ -0,0 +1,6 @@
+local LSM = LibStub('LibSharedMedia-3.0')
+local AddOnName = ...
+local Path = [[Interface\AddOns\]]..AddOnName..[[\Media\]]
+
+LSM:Register('font', 'ArialTest', Path..[[Fonts\Arial.ttf]])
+
